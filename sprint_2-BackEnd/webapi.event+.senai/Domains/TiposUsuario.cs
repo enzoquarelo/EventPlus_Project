5954c9ec904df9b4d4e49545945be8dbc97401fe
enzoquarelo <enzo.quarelo@gmail.com>
2023-11-01 10:47:04 -0300

    update folder

---
+++ sprint_2-BackEnd/webapi.event+.senai/Domains/TiposUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.event_.senai.Domains
+{
+    [Table(nameof(TiposUsuario))]
+    public class TiposUsuario
+    {
+        [Key]
+        public Guid IdTiposUsuario { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage ="Título do usuário obrigatório!")]
+        public string? Titulo { get; set; }
+    }
+}
