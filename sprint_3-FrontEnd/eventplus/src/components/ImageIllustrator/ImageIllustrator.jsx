616b4fdc0ecfd55212b6840f443a7706aa94f258
enzoquarelo <enzo.quarelo@gmail.com>
2023-11-13 08:44:30 -0300

    ...

---
+++ sprint_3-FrontEnd/eventplus/src/components/ImageIllustrator/ImageIllustrator.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './ImageIllustrator.css'
+
+const ImageIllustrator = ({ alteText, additionalClass = "", imageRender}) => {
+
+    return(
+        <figure className="illustrator-box">
+            <img 
+                src={imageRender} 
+                alt={alteText}
+                className={`illustrator-box__image ${additionalClass}`} 
+            />
+        </figure>
+    );
+}
+
+export default ImageIllustrator;
--- sprint_3-FrontEnd/eventplus/src/pages/TipoEventosPage/TipoEventosPage.jsx
+++ sprint_3-FrontEnd/eventplus/src/pages/TipoEventosPage/TipoEventosPage.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./TipoEventosPage.css";
+
+import MainContent from "../../components/Main/MainContent";
+import ImageIllustrator from "../../components/ImageIllustrator/ImageIllustrator"
+import tipoEventoImage from "../../assets/images/tipo-evento.svg"
 import Titulo from "../../components/Titulo/Titulo";
+import Container from "../../components/Container/Container";
 
 const TipoEventosPage = () => {
   return (
-    <div>
-      <Titulo
-        titleText={"Tipos Evento"}
-        color="#/"
-        additionalClass="margim-acima"
-      />
-    </div>
+    <>
+      <MainContent>
+        <section className="cadastro-evento-section">
+          <Container>
+              <div className="cadastro-evento-box">
+                <Titulo titleText={"Cadastro Tipo de Eventos"} />
+
+                <ImageIllustrator imageRender={tipoEventoImage}/>
+
+                <form action="ftipo-evento">
+                  <p>Formulário será criado aqui</p>
+                </form>
+              </div>
+          </Container>
+        </section>
+      </MainContent>
+    </>
   );
 };
 
